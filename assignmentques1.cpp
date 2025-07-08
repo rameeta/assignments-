b6e89369f0ac03086b99262ce77a52b8c5f1105a
reeree <rameeta.2507@gmail.com>
2025-07-08 11:48:33 +0530

    Update and rename assignmentques.cpp to assignmentques1.cpp

--- assignmentques1.cpp
+++ assignmentques1.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // 1 question :  find missing number from 1 to n :
-/*int findMissingNumber(vector<int>& nums, int n){
+int findMissingNumber(vector<int>& nums, int n){
     int xorAll =0;
     int xorArray =0;
     //Xor all elements from 1 to n
@@ -38,38 +38,8 @@ using namespace std;
 
    return 0;
 
-}*/
-
-
-
-//2 Left rotate the array d places 
-void rotateLeft(vector<int>& nums, int d){
-    int n = nums.size();
-    d %= n;
-    //step by step reverse
-    reverse(nums.begin(), nums.begin()+d);
-    reverse(nums.begin() +d, nums.end());
-    reverse(nums.begin(), nums.end()); 
 }
-int main(){
-    int n ;
-    cout << "enter no of elements" << endl;
-    cin >> n;
-    vector<int> nums(n);
-    cout << " enter the elements " << endl;
-    for(int i =0; i < n; i++){
-        cin >> nums[i];
-    }
-    int d;
-    cout << " enter the rotation point" << endl;
-    cin >>d;
 
-    rotateLeft(nums, d);
 
-    cout << " array after rotation " << endl;
-    for(int num: nums){
-        cout << num << " ";
-    }
-cout << endl;
-}
+
 
